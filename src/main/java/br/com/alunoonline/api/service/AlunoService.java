5fd6576e091f2636671c276e59d30965a736fe70
felipheblatt <felipheblatt@gmail.com>
2025-04-15 20:49:44 -0300

    Feature(main): Adição da funcionalidade de atualizar o aluno no BD.

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -3,8 +3,12 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.management.relation.RelationServiceNotRegisteredException;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,5 +32,25 @@ public class AlunoService {
 
   public void deletarAlunoPorId(Long id) {
     alunoRepository.deleteById(id);
+  }
+
+  public void atualizarAlunoPorId(Long id, Aluno aluno) {
+
+    Optional<Aluno> alunoExistente = alunoRepository.findById(id);
+
+    if (alunoExistente.isEmpty()) {
+      throw new ResponseStatusException(
+              HttpStatus.NOT_FOUND,
+              "Aluno não encontrado com o ID: " + id
+      );
+    }
+
+    Aluno alunoAtualizado = alunoExistente.get();
+
+    alunoAtualizado.setNome(aluno.getNome());
+    alunoAtualizado.setCpf(aluno.getCpf());
+    alunoAtualizado.setEmail(aluno.getEmail());
+
+    alunoRepository.save(alunoAtualizado);
   }
 }
